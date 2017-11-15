1fbad608e2054fc4cbdf7d3baae9af93d77ce421
MrLi <mrli_alex@163.com>
2017-11-15 19:25:35 +0800

    fixed

--- second/main_nav.js
+++ second/main_nav.js
@@ -1,4 +1,4 @@
 var simpleGame = document.getElementsByTagName('ul')[0].firstElementChild.getElementsByTagName('div')[0];
 simpleGame.onclick = function() {
-  window.location.href = "http://127.0.0.1:8080/init.html";
+  window.location.href = "init.html";
 }
