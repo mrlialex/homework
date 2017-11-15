bc71ede47651b4b9056643e79556525b6cec755d
MrLi <mrli_alex@163.com>
2017-11-15 19:29:15 +0800

    fixed

--- second/main_nav.js
+++ second/main_nav.js
@@ -1,4 +1,4 @@
 var simpleGame = document.getElementsByTagName('ul')[0].firstElementChild.getElementsByTagName('div')[0];
 simpleGame.onclick = function() {
-  window.location.href = "init.html";
+  window.location.href = "http://htmlpreview.github.io/?https://github.com/mrlialex/homework/blob/master/second/init.html";
 }
